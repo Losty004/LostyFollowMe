8588f20f17aa39877f6501bf8aa1e57fb522304c
Losty004 <150873890+Losty004@users.noreply.github.com>
2024-08-03 14:00:34 -0600

    Delete LostyFollowMe-main directory

--- LostyFollowMe-main/niveles/muerte.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-func _ready():
-	self.body_entered.connect(_on_body_entered)
-
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().reload_current_scene()
-	
